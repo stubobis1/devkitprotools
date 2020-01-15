8d316c62626bb91480d78e87bb04505574ba0005
StuBob <mr.stubob@gmail.com>
2020-01-15 06:04:27 -0700

    host network

--- devkitpro-docker-shell.sh
+++ devkitpro-docker-shell.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -v --net=host $(pwd):/app devkitpro/devkita64 bin/bash -c 'cd /app; exec "${SHELL:-sh}"'
+docker run -it --rm -v --network host $(pwd):/app devkitpro/devkita64 bin/bash -c 'cd /app; exec "${SHELL:-sh}"'
