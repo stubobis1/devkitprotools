fb1476853d88bb590dac56114cfa972920a583d9
StuBob <mr.stubob@gmail.com>
2020-01-15 14:57:06 -0700

    added support for symlinks

--- yuzu-run-wsl.sh
+++ yuzu-run-wsl.sh
@@ -3,8 +3,7 @@ CMD_YUZU_PATH='C:\Users\StuBob\AppData\Local\yuzu\yuzu-windows-msvc\yuzu-cmd.exe
 BUILD_NAME='app.nro'
 
 pwd-dos(){
-        echo `pwd` | sed -e 's./.\\.g' -e 's.\\c.C:.g'
+        echo `pwd -P` | sed -e 's./.\\.g' -e 's.\\c.C:.g'
 }
-
 
 cmd.exe /k "$CMD_YUZU_PATH $(pwd-dos)\\$BUILD_NAME 2> NUL & exit"
